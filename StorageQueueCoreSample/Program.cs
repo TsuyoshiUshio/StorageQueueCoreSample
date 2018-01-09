f503a98152a264775fef0c39a70d56ca0d473249
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-01-10 00:11:28 +0900

    Add project files.

---
+++ StorageQueueCoreSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StorageQueueCoreSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
